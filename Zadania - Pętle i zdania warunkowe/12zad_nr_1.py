3e8f9af9616cffd93eff09b80ca177728356763e
wikmol <wiktor300397@gmail.com>
2020-07-20 21:00:15 +0200

    Next change

---
+++ Zadania - Pętle i zdania warunkowe/12zad_nr_1.py
@@ -0,0 +1,6 @@
+list = []
+for r in range(1500,2701):
+  if r%7 == 0 and r%5 == 0:
+    list.append(r)
+    
+print(list)
---
+++ Zadania - lambda/14zad_nr_1.py
@@ -0,0 +1,4 @@
+r = lambda x : x  + 15
+print(r(2))
+l = lambda x, y : x*y
+print(lambda)
